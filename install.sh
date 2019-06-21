24a364ce08e91b2094069444596b09cd6624cbdf
cupcakearmy <nicco.borgioli@gmail.com>
2019-06-21 12:22:39 +0200

    install shell script

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+
 OUT_FILE=/usr/local/bin/autorestic
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    TYPE=linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    TYPE=macos
+else
+    echo "Unsupported OS"
+    exit
+fi
+
 curl -s https://api.github.com/repos/cupcakearmy/autorestic/releases/latest \
-| grep "browser_download_url.*-macos" \
+| grep "browser_download_url.*_${TYPE}" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -q -O $OUT_FILE -i -
-chmod +x $OUT_FILE
+| wget -O ${OUT_FILE} -i -
+chmod +x ${OUT_FILE}
+
+autorestic
