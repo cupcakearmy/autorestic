33be94ba554c407e93b5b4183d847158dcfdbd44
cupcakearmy <hi@nicco.io>
2021-04-17 15:12:44 +0200

    licensing

--- cmd/restore.go
+++ cmd/restore.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -23,7 +8,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// restoreCmd represents the restore command
 var restoreCmd = &cobra.Command{
 	Use:   "restore",
 	Short: "Restore backup for location",
@@ -50,7 +34,6 @@ func init() {
 	restoreCmd.Flags().BoolP("force", "f", false, "Force, target folder will be overwritten")
 	restoreCmd.Flags().String("from", "", "Which backend to use")
 	restoreCmd.Flags().String("to", "", "Where to restore the data")
-	// restoreCmd.MarkFlagRequired("to")
 	restoreCmd.Flags().StringP("location", "l", "", "Location to be restored")
 	restoreCmd.MarkFlagRequired("location")
 }
