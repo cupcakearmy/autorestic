823977a91189723f704314aad56b3805ce86eb50
Boris Bera <beraboris@gmail.com>
2024-10-16 20:09:48 -0400

    fix(cron): crash when errors are encountered during a backup

--- internal/cron.go
+++ internal/cron.go
@@ -1,12 +1,22 @@
 package internal
 
+import (
+	"errors"
+	"fmt"
+)
+
 func RunCron() error {
 	c := GetConfig()
+	var errs []error
 	for name, l := range c.Locations {
 		l.name = name
 		if err := l.RunCron(); err != nil {
-			return err
+			errs = append(errs, err)
 		}
+	}
+
+	if len(errs) > 0 {
+		return fmt.Errorf("Encountered errors during cron process:\n%w", errors.Join(errs...))
 	}
 	return nil
 }
