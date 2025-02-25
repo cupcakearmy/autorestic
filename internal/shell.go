a01aea5272196bf2e86fc2fa3daad22ee892ad01
Martin Smith <Martin2112@users.github.com>
2025-02-25 16:03:00 +0000

    Fix typo.

--- internal/shell.go
+++ internal/shell.go
@@ -5,7 +5,7 @@ package internal
 import "os/exec"
 
 const shellName string = "bash"
-const shellArg string = "-c"
+const shellArgs string = "-c"
 
 func execCommand(cmd string, args ...string) *exec.Cmd {
 	return exec.Command(cmd, args...)
